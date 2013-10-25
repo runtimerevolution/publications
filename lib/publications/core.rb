520ce26d4b7f2795542dd298e1409d7d48dbc74b
Alexandre Quiterio <a.quiterio@karma-consulting.pt>
2013-10-25 15:28:11 +0100

    update logic of activation

--- lib/publications/core.rb
+++ lib/publications/core.rb
@@ -11,9 +11,10 @@ module Publications
 
     def self.included(base)
       base.class_eval do
-        scope :active,          -> { active_by_flag.active_by_dates }
+        scope :active,          -> { active_by_dates.active_by_flag }
         scope :active_by_flag,  -> { where(:active => true) }
-        scope :active_by_dates, -> { where("#{self.table_name}.disabled_at IS NULL OR (#{self.table_name}.activated_at >= ? AND #{self.table_name}.disabled_at > ?)",
+        scope :active_by_dates, -> { where("(#{self.table_name}.activated_at <= ? AND #{self.table_name}.disabled_at IS NULL) OR (#{self.table_name}.activated_at IS NULL AND #{self.table_name}.disabled_at IS NULL) OR (#{self.table_name}.activated_at <= ? AND #{self.table_name}.disabled_at > ?)",
+          Time.now.utc+Time.now.utc_offset,
           Time.now.utc+Time.now.utc_offset,
           Time.now.utc+Time.now.utc_offset)
         }
