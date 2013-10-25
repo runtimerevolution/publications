a50a52ef77ea9ccd4fbf1b6a59acc32a723f89a6
Alexandre Quiterio <a.quiterio@karma-consulting.pt>
2013-10-25 14:29:09 +0100

    update query

--- lib/publications/core.rb
+++ lib/publications/core.rb
@@ -13,7 +13,7 @@ module Publications
       base.class_eval do
         scope :active,          -> { active_by_flag.active_by_dates }
         scope :active_by_flag,  -> { where(:active => true) }
-        scope :active_by_dates, -> { where("#{self.table_name}.activated_at > ? OR #{self.table_name}.disabled_at < ?",
+        scope :active_by_dates, -> { where("#{self.table_name}.disabled_at IS NULL OR (#{self.table_name}.activated_at >= ? AND #{self.table_name}.disabled_at > ?)",
           Time.now.utc+Time.now.utc_offset,
           Time.now.utc+Time.now.utc_offset)
         }
